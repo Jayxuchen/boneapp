cf6bfae5a77e350e7c46aef61e2de3e43adb348a
Felix <felixzzz06@gmail.com>
2019-01-12 17:51:09 -0800

    FZ Change html template inheritance

--- core/app.py
+++ core/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('home.html')
 
+@app.route('/result')
+def result():
+    return render_template('result.html')
