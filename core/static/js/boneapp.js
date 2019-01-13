247e27faffb0fd947a0ae58665b99b7b6dff2c42
Tony Sun <tonysun@cs.ucsb.edu>
2019-01-13 01:38:09 -0800

    fixed second page for mad gab output

--- core/static/js/boneapp.js
+++ core/static/js/boneapp.js
@@ -8,16 +8,15 @@ function newWord() {
 
 function saveInput() {
     var input = document.getElementById("userInput").value;
-    document.getElementById("check").innerText= "New Word";
+    document.getElementById("check").innerText= "New Phrase";
     document.getElementById("check").setAttribute("onClick", "javascript: newWord();");
-    if (input == "egregious") {
-    	document.getElementById("userInput").style.border="3.5px solid #85e085";
-    	document.getElementById("incorrect").style.display = "none";
-    	document.getElementById("correct").style.display = "block";
-    }
-    else {
-    	document.getElementById("userInput").style.border="3.5px solid #ff4d4d";
-    	document.getElementById("correct").style.display = "none";
-    	document.getElementById("incorrect").style.display = "block";
-    }
+    document.getElementById("result1").style.display = "block";
+    document.getElementById("result2").style.display = "block";
+
+    	// document.getElementById("userInput").style.border="3.5px solid #85e085";
+    	// document.getElementById("incorrect").style.display = "none";
+    	// document.getElementById("correct").style.display = "block";
+    	// document.getElementById("userInput").style.border="3.5px solid #ff4d4d";
+    	// document.getElementById("correct").style.display = "none";
+    	// document.getElementById("incorrect").style.display = "block";
 }
