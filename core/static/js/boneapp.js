246d26a3b24ab5267fd18aaee4469814532b40fb
Tony Sun <tonysun@cs.ucsb.edu>
2019-01-13 00:07:47 -0800

    updated multiple files

--- core/static/js/boneapp.js
+++ core/static/js/boneapp.js
@@ -1,6 +1,15 @@
+function newWord() {
+	document.getElementById("incorrect").style.display = "none";
+	document.getElementById("correct").style.display = "none";
+	document.getElementById("check").innerText= "Check";
+	document.getElementById("check").setAttribute("onClick", "javascript: saveInput();");
+	document.getElementById("yourWord").innerText= "Your Word: K ER N AH L";
+}
+
 function saveInput() {
     var input = document.getElementById("userInput").value;
     document.getElementById("check").innerText= "New Word";
+    document.getElementById("check").setAttribute("onClick", "javascript: newWord();");
     if (input == "egregious") {
     	document.getElementById("userInput").style.border="3.5px solid #85e085";
     	document.getElementById("incorrect").style.display = "none";
