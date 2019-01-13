26d91da745f216438b32f4a2cdb70c1f207d7051
Tony Sun <tonysun@cs.ucsb.edu>
2019-01-13 02:12:35 -0800

    updated phrase generator

--- core/static/js/boneapp.js
+++ core/static/js/boneapp.js
@@ -1,17 +1,22 @@
-function newWord() {
-	document.getElementById("incorrect").style.display = "none";
-	document.getElementById("correct").style.display = "none";
-	document.getElementById("check").innerText= "Check";
-	document.getElementById("check").setAttribute("onClick", "javascript: saveInput();");
-	document.getElementById("yourWord").innerText= "Your Word: K ER N AH L";
-}
+// function newWord() {
+// 	document.getElementById("result1").style.display = "none";
+// 	document.getElementById("result2").style.display = "none";
+// 	document.getElementById("check").innerText= "Generate";
+// 	document.getElementById("check").setAttribute("onClick", "javascript: saveInput();");
+// 	document.getElementById("actualWord").innerText= "K ER N AH L";
+// }
 
 function saveInput() {
     var input = document.getElementById("userInput").value;
-    document.getElementById("check").innerText= "New Phrase";
-    document.getElementById("check").setAttribute("onClick", "javascript: newWord();");
-    document.getElementById("result1").style.display = "block";
-    document.getElementById("result2").style.display = "block";
+    document.getElementById("check").innerText= "New Phrase!";
+    document.getElementById("userInput").value = '';
+    //document.getElementById("check").setAttribute("onClick", "javascript: newWord();");
+    document.getElementById("result1").style.visibility = "visible";
+    document.getElementById("result2").style.visibility = "visible";
+
+    	
+    	document.getElementById("result1").innerHTML= "A Screwdriver";
+    	document.getElementById("result2").innerHTML= "Ask Rude Arrive Her";
 
     	// document.getElementById("userInput").style.border="3.5px solid #85e085";
     	// document.getElementById("incorrect").style.display = "none";
