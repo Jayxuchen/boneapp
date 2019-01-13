4303b43b8b46ece41175255245dca2ea5a54a39d
Tony Sun <tonysun@cs.ucsb.edu>
2019-01-12 21:07:26 -0800

    FZ Change js file

--- core/static/js/boneapp.js
+++ core/static/js/boneapp.js
@@ -1,11 +1,14 @@
 function saveInput() {
     var input = document.getElementById("userInput").value;
+    document.getElementById("check").innerText= "New Word";
     if (input == "egregious") {
     	document.getElementById("userInput").style.border="3.5px solid #85e085";
-    	document.getElementById("check").style.text = "New Word";
+    	document.getElementById("incorrect").style.display = "none";
+    	document.getElementById("correct").style.display = "block";
     }
     else {
     	document.getElementById("userInput").style.border="3.5px solid #ff4d4d";
-    	text.style.display("wrong");
+    	document.getElementById("correct").style.display = "none";
+    	document.getElementById("incorrect").style.display = "block";
     }
 }
